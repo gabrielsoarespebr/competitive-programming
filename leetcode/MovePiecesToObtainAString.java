ae7a1012a6f9e69bac1b286563fd1aaf9db251b5
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2024-12-05 18:10:56 -0300

    draft: [v02] LeetCode - Move Pieces to Obtain a String (Time Limit Exceeded)

--- leetcode/MovePiecesToObtainAString.java
+++ leetcode/MovePiecesToObtainAString.java
@@ -1,6 +1,6 @@
 // platform: leetcode - https://leetcode.com/problems/move-pieces-to-obtain-a-string
 // programming language: Java
-// status: In progress
+// status: Time Limit Exceeded
 
 // author: Gabriel Soares
 // https://www.linkedin.com/in/gabrielsoarespebr/
@@ -10,12 +10,15 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class MovePiecesToObtainAString {
-    public static void canChange(String start, String target) {
+
+    public static boolean canChange(String start, String target) {
         int lAmount = 0;
+        int lAmountEnd = 0;
         List<Integer> lStartIdList = new ArrayList<>();
         List<Integer> lTargetIdList = new ArrayList<>();
 
         int rAmount = 0;
+        int rAmountEnd = 0;
         List<Integer> rStartIdList = new ArrayList<>();
         List<Integer> rTargetIdList = new ArrayList<>();
 
@@ -28,6 +31,7 @@ public class MovePiecesToObtainAString {
                 lStartIdList.add(i);
             }
             if (target.charAt(i) == 'L') {
+                lAmountEnd++;
                 lTargetIdList.add(i);
             }
 
@@ -37,6 +41,7 @@ public class MovePiecesToObtainAString {
                 rStartIdList.add(i);
             }
             if (target.charAt(i) == 'R') {
+                rAmountEnd++;
                 rTargetIdList.add(i);
             }
 
@@ -46,18 +51,59 @@ public class MovePiecesToObtainAString {
             }
         }
 
-        System.out.println("L: " + lAmount);
-        System.out.println("L start id: " + lStartIdList);
-        System.out.println("L start id: " + lTargetIdList);
-        System.out.println();
-        System.out.println("R: " + rAmount);
-        System.out.println("R start id: " + rStartIdList);
-        System.out.println("R target id: " + rTargetIdList);
-        System.out.println();
-        System.out.println("_ start id: " + emptyStartIdList);
+        if (lAmount != lAmountEnd) return false;
+        if (rAmount != rAmountEnd) return false;
+
+        // LEFT
+        for(int i=0;i<lAmount;i++){
+            int moveAmount = lStartIdList.get(i) - lTargetIdList.get(i);
+
+            // negative moveAmount means wrong moves
+            if (moveAmount < 0) return false;
+
+            for(int j = 0; j < moveAmount; j++){
+                int numberPrevious = lStartIdList.get(i) - 1;
+
+                int indexPrevious = emptyStartIdList.indexOf(numberPrevious);
+
+                // check if left position is empty
+                if (indexPrevious != -1){
+                    emptyStartIdList.set(indexPrevious, lStartIdList.get(i));
+                    lStartIdList.set(i,numberPrevious);
+                } else{
+                    return false;
+                }
+            }
+        }
+
+        // RIGHT
+        for(int i=rAmount-1;i>=0;i--){
+            int moveAmount = rTargetIdList.get(i) - rStartIdList.get(i);
+
+            // negative moveAmount means wrong moves
+            if (moveAmount < 0) return false;
+
+            for(int j = 0; j < moveAmount; j++){
+                int numberNext = rStartIdList.get(i) + 1;
+
+                int indexNext = emptyStartIdList.indexOf(numberNext);
+
+                // check if right position is empty
+                if (indexNext != -1){
+                    emptyStartIdList.set(indexNext, rStartIdList.get(i));
+                    rStartIdList.set(i,numberNext);
+                } else{
+                    return false;
+                }
+            }
+        }
+        
+        return true;
     }
 
     public static void main(String[] args) {
-        canChange("_L__R__R_", "L______RR");
+        
+        boolean result = canChange("_R", "R_");
+        System.out.println(result);
     }
 }
